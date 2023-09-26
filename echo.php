bc164335284f503fbd6abaa21e9e352b1e7555cc
Sacchi4649 <sanyadika@gmail.com>
2023-09-26 10:03:47 +0700

    praktek echo & pertanyaan done

---
+++ echo.php
@@ -0,0 +1,11 @@
+<?php
+// mencetak nilai langsung
+echo "Ini string <br>";
+
+//mencetak nilai variabel
+$nama = "POLNEP";
+$tahun = 2020;
+echo '$nama berdiri pada tahun $tahun <br>';
+?>
+
+//jika petik 1 maka outputnya berupa string dan tidak bisa mencetak nilai variabel
