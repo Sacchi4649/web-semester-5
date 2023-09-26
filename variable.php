f0cddda93546687104de74a0842d05e53dd2dfa8
Sacchi4649 <sanyadika@gmail.com>
2023-09-26 09:50:54 +0700

    menjawab pertanyaan pada nomor 2

--- variable.php
+++ variable.php
@@ -2,3 +2,7 @@
 $nama = 'Coders';
 echo "Halo $nama";
 ?>
+
+//$1nama= "Student"; menghasilkan pesan error karena nama variabel tidak boleh diawali dengan angka
+//$-nama= "Student"; menghasilkan pesan error karena nama variabel tidak bisa dimulai dengan dash, melainkan underscore
+//$_nama = "Coders"; menghasilkan pesan error karena echo menampilkan variabel yang tidak terdefinisi
