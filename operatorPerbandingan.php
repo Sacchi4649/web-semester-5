99fdb5df250615355851ffdaede17e7c6ff9a544
Sacchi4649 <sanyadika@gmail.com>
2023-09-26 13:09:23 +0700

    praktek operator perbandingan done

---
+++ operatorPerbandingan.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = 20;
+$y = "20";
+var_dump($x != $y);
+?>
+
+<!-- jika nilai y adalah string dan operator pembanding adalah == maka nilainya true sebab nilai nya sama2 20,
+ tapi jika operator pembandingnya adalah === maka false sebab tipe data berbeda -->
