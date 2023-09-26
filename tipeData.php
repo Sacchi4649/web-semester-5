58dfed88194cf41d71a42ebf1f97a99444481e61
Sacchi4649 <sanyadika@gmail.com>
2023-09-26 10:32:04 +0700

    praktek tipe data done

---
+++ tipeData.php
@@ -0,0 +1,12 @@
+<?php
+$message = "Halo Student!";
+$lightspeed = 299792458;
+$pi = 3.14159265;
+$jomblo = TRUE;
+$goldarah = array('A', 'B', 'AB', 'O');
+$kosong;
+
+print_r($message)
+    ?>
+
+<!-- var_dump() menampilkan nilai variabel serta tipe datanya -->
