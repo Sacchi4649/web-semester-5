a5dfd341b8bf108147edf2576c1b14d0b9572d2f
Sacchi4649 <sanyadika@gmail.com>
2023-10-04 11:02:55 +0700

    percobaan kondisi if done

--- Struktur Kondisi dan Perulangan/kondisiIf.php
+++ Struktur Kondisi dan Perulangan/kondisiIf.php
@@ -1,4 +1,15 @@
 <?php
 $user = array('user_id' => 1, 'username' => 'linds');
+
 //user=array(1,'linds');
+
+// echo $user['username'];
+
+if (is_array($user)) {
+    $user_id = isset($user['user_id']) ? $user['user_id'] : null;
+    $username = isset($user['username']) ? $user['username'] : null;
+    //statemen lainnya
+}
+echo $user_id;
+echo $username;
 ?>
