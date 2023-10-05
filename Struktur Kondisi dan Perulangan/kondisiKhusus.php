5123d152913edb7ae95c3af783161d4b2c551a6b
Sacchi4649 <sanyadika@gmail.com>
2023-10-05 15:12:07 +0700

    modified:   Struktur Kondisi dan Perulangan/kondisiKhusus.php

--- Struktur Kondisi dan Perulangan/kondisiKhusus.php
+++ Struktur Kondisi dan Perulangan/kondisiKhusus.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('Asia/Jakarta');
 $cekKabisat = date('Y');
 $timestamp = date('d-M-Y h:i:s');
-echo $timestamp . "\n";
+echo $timestamp . "<br>";
 $kabisat = ($cekKabisat % 4 == 0) ? "Kabisat" : "Bukan Kabisat";
 echo "Tahun " . $cekKabisat . " " . $kabisat;
 ?>
