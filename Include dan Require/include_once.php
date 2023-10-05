aec818b66a88ef00ba86ce2d33719d15e28de825
Sacchi4649 <sanyadika@gmail.com>
2023-10-06 00:36:01 +0700

    modified:   Include dan Require/include_once.php

--- Include dan Require/include_once.php
+++ Include dan Require/include_once.php
@@ -1,7 +1,6 @@
 <?php
+include("include_latihan.php");
+include("include_latihan.php");
 include_once("include_latihan.php");
-include("include_latihan.php");
-include("include_latihan.php");
-
 ?>
 <p>Ini adalah halaman utama</p>
