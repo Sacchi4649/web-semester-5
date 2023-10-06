fea4d8e7da2b3c42e1df339af3f5aa43c98a55f7
Sacchi4649 <sanyadika@gmail.com>
2023-10-06 10:28:01 +0700

    form 06 done

---
+++ Penanganan Form/checkbox_proses.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['Pilih'])) {
+    echo "Warna Favorit Anda adalah : <br>";
+}
+if (isset($_POST['warna01'])) {
+    echo "+ " . $_POST['warna01'] . "<br>";
+}
+if (isset($_POST['warna02'])) {
+    echo "+ " . $_POST['warna02'] . "<br>";
+}
+if (isset($_POST['warna03'])) {
+    echo "+ " . $_POST['warna03'] . "<br>";
+}
+if (isset($_POST['warna04'])) {
+    echo "+ " . $_POST['warna04'] . "<br>";
+}
+?>
---
+++ Penanganan Form/form06.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Warna Favorit ~ Inputan Checkbox</title>
+</head>
+
+<body>
+    <form action="checkbox_proses.php" method="post" name="input">
+        <h2>Pilih Warna Favorit Anda :</h2>
+        <input type="checkbox" name="warna01" value="Putih" checked>Putih <br>
+        <input type="checkbox" name="warna02" value="Biru">Biru <br>
+        <input type="checkbox" name="warna03" value="Merah">Merah <br>
+        <input type="checkbox" name="warna04" value="Ungu">Ungu <br>
+        <input type="submit" name="Pilih" value="Pilih">
+    </form>
+</body>
+
+</html>
