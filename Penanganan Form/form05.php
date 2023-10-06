eb9e5c2df9f0389689c8af60beffb95d45e071ed
Sacchi4649 <sanyadika@gmail.com>
2023-10-06 08:52:30 +0700

    form 05 done

---
+++ Penanganan Form/form05.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Pilih Jurusan</title>
+</head>
+
+<body>
+    <form action="form05_proses.php" method="post" name="input">
+        <h2>Pilih Jurusan Anda</h2>
+        <input type="radio" name="jurusan" value="TI" checked>Teknik Informatika <br>
+        <input type="radio" name="jurusan" value="SI">Sistem Informasi <br>
+        <input type="radio" name="jurusan" value="SK">Sistem Komputer <br>
+        <input type="radio" name="jurusan" value="KA">Komputerisasi Akuntansi <br>
+        <input type="submit" name="pilih" value="Pilih">
+    </form>
+</body>
+
+</html>
