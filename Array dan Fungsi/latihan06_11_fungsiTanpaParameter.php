93c1f9c25312b2e8334234816d2cb92dd1d545c2
Sacchi4649 <sanyadika@gmail.com>
2023-10-09 15:19:10 +0700

    latihan fungsi tanpa parameter done

---
+++ Array dan Fungsi/latihan06_11_fungsiTanpaParameter.php
@@ -0,0 +1,13 @@
+<?php
+// pertama kita terlebih dahulu mendefinisikan fungsinya,
+// kemudian mengisi fungsi tersebut dengan sederetan perintah
+
+function testing()
+{
+    echo "Contoh menggunakan Fungsi";
+}
+
+// untuk dapat menggunakan fungsi yang telah dibuat,
+// perlu pemanggilan nama fungsinya
+testing();
+?>
