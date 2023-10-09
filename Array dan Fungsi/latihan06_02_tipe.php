55b4962383207181f7ed0911cdbc08c7c9bba0a5
Sacchi4649 <sanyadika@gmail.com>
2023-10-09 11:36:58 +0700

    latihan 2 tipe done

---
+++ Array dan Fungsi/latihan06_02_tipe.php
@@ -0,0 +1,10 @@
+<?php
+$mahasiswa = array("Lindung Siswanto", "Laki-laki", "11/06/1984", "A", 3.51, "Pemrograman Web");
+
+echo $mahasiswa[0] . "<br>";
+echo $mahasiswa[1] . "<br>";
+echo $mahasiswa[2] . "<br>";
+echo $mahasiswa[3] . "<br>";
+echo $mahasiswa[4] . "<br>";
+echo $mahasiswa[5] . "<br>";
+?>
