ab1eb23e68d7ee74e5f2fba998a186ead7ead6cf
Sacchi4649 <sanyadika@gmail.com>
2023-10-09 12:51:40 +0700

    latihan array pisah done

---
+++ Array dan Fungsi/latihan06_04_array_pisah.php
@@ -0,0 +1,12 @@
+<?php
+$mahasiswa = array("Lindung Siswanto", "Laki-laki", "11/06/1984", "A", 3.51, "Pemrograman Web");
+
+list($nama, $jeniskelamin, $tanggallahir, $poin, $IP, $spesialisasi) = $mahasiswa;
+
+echo $nama . "<br>";
+echo $jeniskelamin . "<br>";
+echo $tanggallahir . "<br>";
+echo $poin . "<br>";
+echo $IP . "<br>";
+echo $spesialisasi . "<br>";
+?>
