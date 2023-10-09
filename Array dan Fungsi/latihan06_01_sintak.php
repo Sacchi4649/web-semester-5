d73bda4a2cab79e0ed9575caa30e277521a1a1a4
Sacchi4649 <sanyadika@gmail.com>
2023-10-09 11:11:14 +0700

    latihan 1 sintak done

---
+++ Array dan Fungsi/latihan06_01_sintak.php
@@ -0,0 +1,12 @@
+<?php
+$tahun = array(2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070);
+// echo $tahun[3];
+
+echo "Tampilkan elemen ke 4 <br>";
+echo "Elemen ke 4: $tahun[3] <br>";
+
+//Tambahkan 3 elemen
+echo " Tampilkan elemen ke 6 <br>";
+echo "Elemen ke 6:  $tahun[5]";
+
+?>
