d04c358adaca4c980086a6bebb7f60b6b41e4568
Sacchi4649 <sanyadika@gmail.com>
2023-10-09 15:15:46 +0700

    latihan array associative done

---
+++ Array dan Fungsi/latihan06_09_array_associative.php
@@ -0,0 +1,21 @@
+<?php
+echo "Associative Array Dimensi Dua: <br>";
+
+$nilai = array(
+    array("IPA" => 90, "IPS" => 65, "Bahasa" => 83),
+    array("IPA" => 90, "IPS" => 78, "Bahasa" => 87),
+    array("IPA" => 78, "IPS" => 90, "Bahasa" => 78)
+);
+
+// Mengakses elemen array dengan looping
+echo "<ul>";
+for ($r = 0; $r < count($nilai); $r++) {
+    echo "<li>";
+    foreach ($nilai[$r] as $key => $value) {
+        echo $key . " = " . $value . " ";
+    }
+    echo "</li>";
+    echo "<br><br";
+}
+echo "</ul>";
+?>
