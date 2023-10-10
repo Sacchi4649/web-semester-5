165b517400734b105695dd47e7e0d1070a8063b2
Sacchi4649 <sanyadika@gmail.com>
2023-10-10 13:28:07 +0700

    latihan 13 done

--- Array dan Fungsi/latihan06_13.php
+++ Array dan Fungsi/latihan06_13.php
@@ -1,13 +1,27 @@
 <?php
-
-function hitung($a, $b)
+function perkalian($a, $b)
 {
     return $a * $b;
 }
-
 function pembagian($a, $b)
 {
-
+    return $a / $b;
 }
-echo "Hasil = " . hitung(2, 4);
+function penambahan($a, $b)
+{
+    return $a + $b;
+}
+function pengurangan($a, $b)
+{
+    return $a - $b;
+}
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+echo "Hasil perkalian= " . perkalian(11, 2) . "<br>";
+echo "Hasil  pembagian= " . pembagian(11, 2) . "<br>";
+echo "Hasil  penambahan= " . penambahan(11, 2) . "<br>";
+echo "Hasil  pengurangan= " . pengurangan(11, 2) . "<br>";
+echo "Hasil  modulus= " . modulus(11, 2) . "<br>";
 ?>
