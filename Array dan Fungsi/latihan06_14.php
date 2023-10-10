9ac215d4c7779260366a1d0e9761ad834dc3d1bd
Sacchi4649 <sanyadika@gmail.com>
2023-10-10 16:01:00 +0700

    update latihan 14

--- Array dan Fungsi/latihan06_14.php
+++ Array dan Fungsi/latihan06_14.php
@@ -3,7 +3,8 @@
 $mahasiswa = ["Lindung Siswanto", "Laki-laki", "11/06/1984", "A", 3.51, "Pemrograman Web"];
 function printArray($array)
 {
-    for ($x = 0; $x <= 5; $x++) {
+    $count = count($array);
+    for ($x = 0; $x < $count; $x++) {
         echo $array[$x] . "<br>";
     }
 }
