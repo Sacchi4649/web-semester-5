05f07ca6ba1f33af24c1267f0df3a6ff4ec8775e
Sacchi4649 <sanyadika@gmail.com>
2023-10-16 12:55:40 +0700

    latihan 2 done

---
+++ String dan Tanggal/simpleHeredoc.php
@@ -0,0 +1,10 @@
+<?php
+$str = <<<MY_TEXT
+Contoh penulisan heredoc pada PHP.
+Menulis string tanpa escape (back slash) pada PHP.
+Contoh kata Jum'at dengan satu tanda kutip atau
+Jum"at dengan dua tanda kutip
+MY_TEXT;
+
+echo $str;
+?>
