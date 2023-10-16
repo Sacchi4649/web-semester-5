5f1c0f834c14794ba0e8618d29889b7bb4e7f144
Sacchi4649 <sanyadika@gmail.com>
2023-10-16 12:42:32 +0700

    latihan 1 done

---
+++ String dan Tanggal/string01.php
@@ -0,0 +1,22 @@
+<?php
+echo 'Penulisan string sederhana';
+echo '<br>';
+echo 'Menuliskan string seperti ini
+bisa juga dilakukan
+tidak mengapa, lakukan saja';
+echo '<br>';
+// Outputs: Syafi'i berkata: "ini hari jum'at""
+echo 'Syafi\'i berkata: "ini hari jum\'at"';
+echo '<br>';
+// Outputs: Menghapus C:\*.*?
+echo 'Menghapus C:\\*.*?';
+echo '<br>';
+// Outputs: Menghapus C:\*.*?
+echo 'Menghapus C:\\*.*?';
+echo '<br>';
+// Outputs: Ini bukan baris baru: \n bukan baris baru
+echo 'Ini bukan baris baru: \n bukan baris baru';
+echo '<br>';
+// Outputs: Menampilkan nama variabel, buka valuenya $var1 $var2 
+echo 'Menampilkan nama variabel, buka valuenya $var1 $var2';
+?>
