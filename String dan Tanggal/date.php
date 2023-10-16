cf45dbb70a17316a667bcb6843171ddc0f746080
Sacchi4649 <sanyadika@gmail.com>
2023-10-16 13:51:26 +0700

    latihan date done

---
+++ String dan Tanggal/date.php
@@ -0,0 +1,15 @@
+<?php
+// Sekarang tanggal 20 Oktober 2021 09:43:49
+
+echo "<br>" . date("d/m/Y H:i:s"); // 27/10/2021 07:03:01
+echo "<br>" . date("F j, Y, g:i a"); // October 27, 2021, 7:03 am
+echo "<br>" . date("d.m.y"); // 27.10.21
+echo "<br>" . date("Ymd"); // 20211027
+// 27-10-21, 0331 0301 3 Wedam21 299
+echo "<br>" . date('j-m-y, it is w Day z ');
+// it is the 27th day.
+echo "<br>" . date('\i\t \i\s \t\h\e js \d\a\y.');
+// Wed Oct 27 07:05:48 CEST 2021
+echo "<br>" . date("D Mj G:i:s T Y");
+echo "<br>" . date("H:i:s"); // 07:05:48
+?>
